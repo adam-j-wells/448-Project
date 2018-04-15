9afc5e5887d1c0e981c2aef7126468f038106d8d
adam-j-wells <awells356@gmail.com>
2018-04-15 15:23:17 -0700

    Added data description

--- featureGenerator.py
+++ featureGenerator.py
@@ -1,3 +1,12 @@
-```
+'''
 Generates features from Order Book data
-```
+Data format:
+    - TO DO
+'''
+import pandas as pd
+import numpy as np
+
+'''
+Local path containing data
+'''
+data_dir = '../448_data/'
