dd87d79a05958240fe23fde43789a3c74ec3297b
adam-j-wells <awells356@gmail.com>
2018-04-16 18:59:42 -0700

    data formatting

--- featureGenerator.py
+++ featureGenerator.py
@@ -1,12 +1,32 @@
 '''
 Generates features from Order Book data
 Data format:
-    - TO DO
+    - data_dir folder contains CSVs with order book data for a given stock
+    (msft, goog, amzn, aapl) at a given depth (1-10)
+    - Order book data is second-by-second
 '''
 import pandas as pd
 import numpy as np
 
 '''
 Local path containing data
+Raw Data Format:
+columns:
+    - datetime (YYYY-MM-DD H:M:S)
+    - bid{n} n=1:10
+    - ask{n} n=1:10
 '''
-data_dir = '../448_data/'
+data_dir = '../ProjectData/'
+
+
+def createFeatures(data_path, out_path):
+    '''
+    Generates features from Order Book Data
+
+    Inputs:
+        - data_path: path to order book data
+        - out_path: path to place generated file
+    Output:
+        - featureMatrix: data frame containing original data and features
+    '''
+    pass
